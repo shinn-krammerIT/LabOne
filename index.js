bcb69ec7b3007f61efce91603ad8fb1162b4da8a
[Shinn Krammer] <[aa9e2d379a5e29cc15ac5aa7953e8ca0c32ac299]>
2020-03-03 15:18:42 +1300

    Made new script. Tidy.

---
+++ index.js
@@ -0,0 +1,12 @@
+var msg="Hello World";
+var firstName="Shinn";
+var lastName="Krammer";
+var fullName=(firstName + " " + lastName);
+
+var currentYear=2020;
+var birthYear=2000;
+var age=currentYear - birthYear;
+
+console.log(msg);
+console.log("My name is " + fullName);
+console.log("I am " + age + " years old");
---
+++ math.js
@@ -0,0 +1,18 @@
+//Math functions + - / % *
+//Global Variables
+var sum;
+var multi;
+var div;
+//values
+let numOne = 9;
+let numTwo = 3;
+
+//formulas
+sum = numOne + numTwo;
+multi = numOne * numTwo;
+div = numOne / numTwo;
+
+//print
+console.log("Sum (+) = " + sum);
+console.log("Multi (*) = " + multi);
+console.log("Div (/) = " + div);
